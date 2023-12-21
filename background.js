89e7a802f1a6d81879d78b7e7024e00315649a76
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2023-12-21 10:21:39 +0000

    removed tabs.query

--- background.js
+++ background.js
@@ -2,20 +2,15 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse)
 {
     if (request.action === 'captureVisibleTab') 
     {
-        chrome.tabs.query({active : true}, function(tab) 
+        // Capture the visible tab
+        chrome.tabs.captureVisibleTab(null, { format: 'png' }, function (data_url) 
         {
-            console.log('Tab id : ', tab.id);
-      
-            // Capture the visible tab
-            chrome.tabs.captureVisibleTab(null, { format: 'png' }, function (data_url) 
-            {
-                // Send the captured image data back to the content script
-                sendResponse({ imageData: data_url });
-                console.log(data_url);
-            });
+            // Send the captured image data back to the content script
+            sendResponse({ imageData: data_url });
+            console.log(data_url);
         });
-
-      // Return true to indicate that sendResponse will be called asynchronously
-      return true;
     }
+    
+    // Return true to indicate that sendResponse will be called asynchronously
+    return true;
 });
